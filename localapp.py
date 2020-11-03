63379b4c8e6fda018337444a8ad2bef524df565a
jite21 <jite21.jks@gmail.com>
2020-11-03 20:15:31 +0530

    Update localapp.py

--- localapp.py
+++ localapp.py
@@ -1,2 +1,11 @@
-import flask
+from flask import Flask
 #testing fork, cmd commit, pull request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'This is response from Localserver'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5001')
